8b3fe2bba9217c3c08872a06b03ef2c19806c81c
Ahsan Zia <ahsan97@hotmail.co.uk>
2019-12-19 13:53:20 +0000

    mangas on the list are clickable and it displays extra details when clicked

--- src/App.js
+++ src/App.js
@@ -1,69 +1,129 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
-import { formatResultsErrors } from 'jest-message-util';
+import Grid from '@material-ui/core/Grid';
+import Rating from 'react-rating';
 
 export default class App extends Component {
 
   constructor(props) {
     super(props)
-  
+
     /**
      * list shows displayed mangas on screen
+     * genre of a manga is stored in array
      * start and end are to show the limit of manga shown on screen
-     */ 
+     * display is false when no manga has been clicked to view
+     */
     this.state = {
-       list: [],
-       start: 0,
-       end: 10
+      list: [],
+      genre: "",
+      display: false,
+      start: 0,
+      end: 10
     }
 
     this.next = this.next.bind(this);
     this.prev = this.prev.bind(this);
+    this.viewManga = this.viewManga.bind(this);
+
   }
-  
+
   componentDidMount() {
     this.getMangas();
   }
 
   // gets all list of mangas
   getMangas = () => {
-    const api = fetch('https://www.mangaeden.com/api/list/1/').then(res => res.json()).then((result) => {
-      const mangas =  result.manga.slice(this.state.start,this.state.end);
-      this.setState({list: mangas.map(manga => manga.t) })
+    fetch('https://www.mangaeden.com/api/list/1/').then(res => res.json()).then((result) => {
+      const mangas = result.manga.slice(this.state.start, this.state.end);
+      var genres = [];
+      mangas.map(function(manga) {
+        var string = "";
+        manga.c.forEach(genre => string += genre + " ");
+        genres.push(string);
+      });
+      this.setState({ 
+        list: mangas.map(manga => manga.t),
+        genre: genres
+      });
     })
   }
 
   // shows the next 10 mangas
   next() {
+    this.reset();
     this.setState(state => ({
       start: state.end,
-      end: state.end+10
+      end: state.end + 10
     }));
     this.getMangas();
   }
 
   // shows the previous 10 mangas
   prev() {
+    this.reset();
     this.setState(state => ({
       end: state.start < 10 ? 10 : state.start,
-      start: state.start-10 < 0 ? 0 : state.start-10
+      start: state.start - 10 < 0 ? 0 : state.start - 10
     }));
     this.getMangas();
-  } 
+  }
+
+  // clicking on a manga in the will show the mangas detail
+  viewManga(index) {
+    this.reset();
+    this.setState({ display: true });
+    console.log(this.state.genre[index.index]);
+    this.setState({
+      displayGenre: this.state.genre[index.index]
+    })
+  }
+
+  // resets some of the variable before an action
+  reset() {
+    this.setState({
+      display: false,
+      displayGenre: ""
+    })
+  }
 
   render() {
 
     return (
-      
+
       <div>
-        <ul>
-          {this.state.list.map(function(name,index){
-            return <li key ={index}> {name}</li>;
-          })}
-        </ul>
+        <div>
+          <Grid spacing={10}>
+            <Grid container item xs={12} spacing={1}>
+
+              <Grid item xs={3}>
+                <ul>
+                  {this.state.list.map(function (name, index) {
+                    return <li key={index} onClick={() => this.viewManga({ index })}> {name}</li>;
+                  }, this)}
+                </ul>
+              </Grid>
+
+              {this.state.display &&
+                <Grid item xs={3}>
+                  <p> <span className="manga_subtitle">Genre: {this.state.displayGenre}</span>  </p>
+                  <p> <span className="manga_subtitle">Ratings:</span>
+                    <Rating
+                      emptySymbol="fa fa-star-o fa-2x"
+                      fullSymbol="fa fa-star fa-2x"
+                      fractions={2}
+                    />
+                  </p>
+                </Grid>
+              }
+
+            </Grid>
+          </Grid>
+        </div>
+        
         <button onClick={this.prev}>Prev</button>
         <button onClick={this.next}>Next</button>
+
       </div>
     )
   }
